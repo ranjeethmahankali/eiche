3dfa62013d774587dac768ac0bd56376a8469e6d
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2023-10-29 01:33:31 -0400

    Add docstrings

--- asg/src/tree.rs
+++ asg/src/tree.rs
@@ -217,17 +217,22 @@ impl<'a> Evaluator<'a> {
         }
     }
 
-    pub fn set_var(&mut self, label: char, val: f64) {
+    /// Set all symbols in the evaluator matching `label` to
+    /// `value`. This `value` will be used for all future evaluations,
+    /// unless this function is called again with a different `value`.
+    pub fn set_var(&mut self, label: char, value: f64) {
         for (node, reg) in self.tree.nodes.iter().zip(self.regs.iter_mut()) {
             match node {
                 Symbol(l) if *l == label => {
-                    *reg = Some(val);
+                    *reg = Some(value);
                 }
                 _ => {}
             }
         }
     }
 
+    /// Read the value from the `index`-th register. Returns an error
+    /// if the register doesn't contain a value.
     fn read(&self, index: usize) -> Result<f64, EvaluationError> {
         match self.regs[index] {
             Some(val) => Ok(val),
@@ -239,6 +244,10 @@ impl<'a> Evaluator<'a> {
         self.regs[index] = Some(value);
     }
 
+    /// Run the evaluator and return the result. The result may
+    /// contain the output value, or an
+    /// error. `Variablenotfound(label)` error means the variable
+    /// matching `label` hasn't been assigned a value using `set_var`.
     pub fn run(&mut self) -> Result<f64, EvaluationError> {
         for idx in 0..self.tree.nodes.len() {
             self.write(
