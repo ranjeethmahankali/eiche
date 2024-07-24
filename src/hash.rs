1468644f8b23ceceb08d6dbf5165fe52f5089a58
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2024-07-23 23:14:14 -0400

    Test for hashing commutative nodes

--- src/hash.rs
+++ src/hash.rs
@@ -65,3 +65,37 @@ impl Tree {
         return s.finish();
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::deftree;
+
+    #[test]
+    fn t_commutative() {
+        let mut hashbuf = Vec::new();
+
+        let t1 = deftree!(+ x y).unwrap();
+        let t2 = deftree!(+ y x).unwrap();
+        assert_eq!(t1.hash(&mut hashbuf), t2.hash(&mut hashbuf));
+
+        let t1 = deftree!(* x y).unwrap();
+        let t2 = deftree!(* y x).unwrap();
+        assert_eq!(t1.hash(&mut hashbuf), t2.hash(&mut hashbuf));
+
+        let t1 = deftree!(- x y).unwrap();
+        let t2 = deftree!(- y x).unwrap();
+        assert_ne!(t1.hash(&mut hashbuf), t2.hash(&mut hashbuf));
+
+        let t1 = deftree!(/ x y).unwrap();
+        let t2 = deftree!(/ y x).unwrap();
+        assert_ne!(t1.hash(&mut hashbuf), t2.hash(&mut hashbuf));
+
+        let t1 = deftree!(/ 1 (- (log (+ x y)) x)).unwrap();
+        let t2 = deftree!(/ 1 (- (log (+ y x)) x)).unwrap();
+        assert_eq!(t1.hash(&mut hashbuf), t2.hash(&mut hashbuf));
+
+        let t1 = deftree!(/ 1 (- (log (/ x y)) x)).unwrap();
+        let t2 = deftree!(/ 1 (- (log (/ y x)) x)).unwrap();
+        assert_ne!(t1.hash(&mut hashbuf), t2.hash(&mut hashbuf));
+    }
+}
