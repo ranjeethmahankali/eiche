a7c3160c312a4586fcc36d333dd2eee47cb62516
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2024-08-26 14:50:46 -0400

    Adjust path

--- src/llvm_jit/single.rs
+++ src/llvm_jit/single.rs
@@ -387,7 +387,7 @@ impl Tree {
                 .get_function(FUNC_NAME)
                 .map_err(|e| Error::JitCompilationError(e.to_string()))?
         };
-        compiler.write_asm(&std::path::Path::new("/mnt/d/dev/linux/eiche")); // temporary code.
+        compiler.write_asm(&std::path::Path::new("/mnt/d/dev/linux/fidgetmark/hex_llvm.asm")); // temporary code.
         return Ok(JitEvaluator::create(func, symbols.len(), num_roots));
     }
 }
