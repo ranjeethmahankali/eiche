ed2f35786a559b0d58aba72d220434a0989d638f
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2024-09-09 12:22:30 -0400

    Write out LLVM IR

--- src/llvm_jit/single.rs
+++ src/llvm_jit/single.rs
@@ -390,6 +390,9 @@ impl Tree {
         compiler.write_asm(&std::path::Path::new(
             "/mnt/d/dev/linux/fidgetmark/hex_llvm.obj",
         )); // temporary code.
+        compiler.write_llvm_ir(&std::path::Path::new(
+            "/mnt/d/dev/linux/fidgetmark/hex_llvm.ir",
+        )); // temporary code.
         return Ok(JitEvaluator::create(func, symbols.len(), num_roots));
     }
 }
