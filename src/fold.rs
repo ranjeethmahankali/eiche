c97915b485c2e2a59defe4056f0b54337bed5f50
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2025-02-16 17:19:27 -0500

    Revert fold stuff

--- src/fold.rs
+++ src/fold.rs
@@ -1,277 +1,77 @@
-use std::collections::BTreeMap;
-
 use crate::{
     error::Error,
     eval::ValueType,
-    inari_interval::Interval,
     tree::{
-        BinaryOp::{self, *},
+        BinaryOp::*,
         Node::{self, *},
-        TernaryOp::{self, *},
+        TernaryOp::*,
         Tree,
-        UnaryOp::{self, *},
+        UnaryOp::*,
         Value::{self, *},
     },
 };
-
-fn fold_unary_op(op: UnaryOp, input: usize, nodes: &[Node]) -> Result<Option<Node>, Error> {
-    Ok(if let Constant(value) = nodes[input] {
-        Some(Constant(Value::unary_op(op, value)?))
-    } else {
-        None
-    })
-}
-
-fn fold_binary_op(
-    op: BinaryOp,
-    li: usize,
-    ri: usize,
-    nodes: &[Node],
-) -> Result<Option<Node>, Error> {
-    Ok(match (op, &nodes[li], &nodes[ri]) {
-        // Constant folding.
-        (op, Constant(a), Constant(b)) => Some(Constant(Value::binary_op(op, *a, *b)?)),
-        // Identity ops.
-        (Add, lhs, Constant(val)) if *val == 0. => Some(*lhs),
-        (Add, Constant(val), rhs) if *val == 0. => Some(*rhs),
-        (Subtract, lhs, Constant(val)) if *val == 0. => Some(*lhs),
-        (Multiply, lhs, Constant(val)) if *val == 1. => Some(*lhs),
-        (Multiply, Constant(val), rhs) if *val == 1. => Some(*rhs),
-        (Pow, base, Constant(val)) if *val == 1. => Some(*base),
-        (Divide, numerator, Constant(val)) if *val == 1. => Some(*numerator),
-        (Or, lhs, Constant(rhs)) if *rhs == false => Some(*lhs),
-        (Or, Constant(lhs), rhs) if *lhs == false => Some(*rhs),
-        (And, lhs, Constant(rhs)) if *rhs == true => Some(*lhs),
-        (And, Constant(lhs), rhs) if *lhs == true => Some(*rhs),
-        // Other ops.
-        (Subtract, Constant(val), _rhs) if *val == 0. => Some(Unary(Negate, ri)),
-        (Pow, _base, Constant(val)) if *val == 0. => Some(Constant(Scalar(1.))),
-        (Multiply, _lhs, Constant(val)) if *val == 0. => Some(Constant(Scalar(0.))),
-        (Multiply, Constant(val), _rhs) if *val == 0. => Some(Constant(Scalar(0.))),
-        (Divide, Constant(val), _rhs) if *val == 0. => Some(Constant(Scalar(0.))),
-        (Or, _lhs, Constant(rhs)) if *rhs == true => Some(Constant(Bool(true))),
-        (Or, Constant(lhs), _rhs) if *lhs == true => Some(Constant(Bool(true))),
-        (And, _lhs, Constant(rhs)) if *rhs == false => Some(Constant(Bool(false))),
-        (And, Constant(lhs), _rhs) if *lhs == false => Some(Constant(Bool(false))),
-        _ => None,
-    })
-}
-
-fn fold_ternary_op(
-    op: TernaryOp,
-    a: usize,
-    b: usize,
-    c: usize,
-    nodes: &[Node],
-) -> Result<Option<Node>, Error> {
-    Ok(match (op, &nodes[a], &nodes[b], &nodes[c]) {
-        (op, Constant(a), Constant(b), Constant(c)) => {
-            Some(Constant(Value::ternary_op(op, *a, *b, *c)?))
-        }
-        (Choose, Constant(flag), left, right) => {
-            if flag.boolean()? {
-                Some(*left)
-            } else {
-                Some(*right)
-            }
-        }
-        _ => None,
-    })
-}
 
 /**
 Compute the results of operations on constants and fold those into
 constant nodes. The unused nodes after folding are not
 pruned. Use a pruner for that.
 */
-pub(crate) fn fold(nodes: &mut [Node]) -> Result<(), Error> {
+pub fn fold_nodes(nodes: &mut [Node]) -> Result<(), Error> {
     for index in 0..nodes.len() {
-        if let Some(node) = match nodes[index] {
+        let folded = match nodes[index] {
             Constant(_) => None,
             Symbol(_) => None,
-            Unary(op, input) => fold_unary_op(op, input, nodes)?,
-            Binary(op, li, ri) => fold_binary_op(op, li, ri, nodes)?,
-            Ternary(op, a, b, c) => fold_ternary_op(op, a, b, c, nodes)?,
-        } {
-            nodes[index] = node;
-        }
-    }
-    Ok(())
-}
-
-pub(crate) fn fold_with_interval(
-    nodes: &mut [Node],
-    vars: &BTreeMap<char, Interval>,
-) -> Result<(), Error> {
-    // This is similar to the folding above, except we use the results of the
-    // interval evaluation instead of hardcoding the rules.
-    let mut values: Vec<Interval> = Vec::with_capacity(nodes.len());
-    for index in 0..nodes.len() {
-        let (folded, value) = match nodes[index] {
-            Constant(val) => (None, Interval::from_value(val)?),
-            Symbol(label) => (
-                None,
-                vars.get(&label)
-                    .copied()
-                    .ok_or(Error::VariableNotFound(label))?,
-            ),
-            Unary(op, input) => match fold_unary_op(op, input, nodes)? {
-                Some(folded) => (
-                    Some(folded),
-                    match &folded {
-                        Constant(value) => Interval::from_value(*value)?,
-                        Symbol(label) => vars
-                            .get(&label)
-                            .copied()
-                            .ok_or(Error::VariableNotFound(*label))?,
-                        Unary(op, input) => Interval::unary_op(*op, values[*input])?,
-                        Binary(op, lhs, rhs) => {
-                            Interval::binary_op(*op, values[*lhs], values[*rhs])?
-                        }
-                        Ternary(op, a, b, c) => {
-                            Interval::ternary_op(*op, values[*a], values[*b], values[*c])?
-                        }
-                    },
-                ),
-                None => {
-                    let tofold = match (op, values[input]) {
-                        (Negate, Interval::Scalar(ii)) => ii.is_singleton() && ii.inf() == 0.,
-                        (Sqrt, Interval::Scalar(ii)) => {
-                            ii.is_singleton() && (ii.inf() == 0. || ii.inf() == 1.)
-                        }
-                        (Abs, Interval::Scalar(ii)) => ii.inf() >= 0.,
-                        (Sin, Interval::Scalar(ii)) => ii.is_singleton() && ii.inf() == 0.,
-                        (Cos, Interval::Scalar(_))
-                        | (Tan, Interval::Scalar(_))
-                        | (Log, Interval::Scalar(_))
-                        | (Exp, Interval::Scalar(_)) => false,
-                        (Floor, Interval::Scalar(ii)) => {
-                            ii.is_singleton() && ii.inf().fract() == 0.
-                        }
-                        (Not, Interval::Boolean(_, _)) => false, // This should be taken care of by folding.
-                        (Negate, Interval::Boolean(_, _))
-                        | (Sqrt, Interval::Boolean(_, _))
-                        | (Abs, Interval::Boolean(_, _))
-                        | (Sin, Interval::Boolean(_, _))
-                        | (Cos, Interval::Boolean(_, _))
-                        | (Tan, Interval::Boolean(_, _))
-                        | (Log, Interval::Boolean(_, _))
-                        | (Exp, Interval::Boolean(_, _))
-                        | (Floor, Interval::Boolean(_, _))
-                        | (Not, Interval::Scalar(_)) => return Err(Error::TypeMismatch),
-                    };
-                    if tofold {
-                        (Some(nodes[input]), values[input])
+            Unary(op, input) => {
+                if let Constant(value) = nodes[input] {
+                    Some(Constant(Value::unary_op(op, value)?))
+                } else {
+                    None
+                }
+            }
+            Binary(op, li, ri) => match (op, &nodes[li], &nodes[ri]) {
+                // Constant folding.
+                (op, Constant(a), Constant(b)) => Some(Constant(Value::binary_op(op, *a, *b)?)),
+                // Identity ops.
+                (Add, lhs, Constant(val)) if *val == 0. => Some(*lhs),
+                (Add, Constant(val), rhs) if *val == 0. => Some(*rhs),
+                (Subtract, lhs, Constant(val)) if *val == 0. => Some(*lhs),
+                (Multiply, lhs, Constant(val)) if *val == 1. => Some(*lhs),
+                (Multiply, Constant(val), rhs) if *val == 1. => Some(*rhs),
+                (Pow, base, Constant(val)) if *val == 1. => Some(*base),
+                (Divide, numerator, Constant(val)) if *val == 1. => Some(*numerator),
+                (Or, lhs, Constant(rhs)) if *rhs == false => Some(*lhs),
+                (Or, Constant(lhs), rhs) if *lhs == false => Some(*rhs),
+                (And, lhs, Constant(rhs)) if *rhs == true => Some(*lhs),
+                (And, Constant(lhs), rhs) if *lhs == true => Some(*rhs),
+                // Other ops.
+                (Subtract, Constant(val), _rhs) if *val == 0. => Some(Unary(Negate, ri)),
+                (Pow, _base, Constant(val)) if *val == 0. => Some(Constant(Scalar(1.))),
+                (Multiply, _lhs, Constant(val)) if *val == 0. => Some(Constant(Scalar(0.))),
+                (Multiply, Constant(val), _rhs) if *val == 0. => Some(Constant(Scalar(0.))),
+                (Divide, Constant(val), _rhs) if *val == 0. => Some(Constant(Scalar(0.))),
+                (Or, _lhs, Constant(rhs)) if *rhs == true => Some(Constant(Bool(true))),
+                (Or, Constant(lhs), _rhs) if *lhs == true => Some(Constant(Bool(true))),
+                (And, _lhs, Constant(rhs)) if *rhs == false => Some(Constant(Bool(false))),
+                (And, Constant(lhs), _rhs) if *lhs == false => Some(Constant(Bool(false))),
+                _ => None,
+            },
+            Ternary(op, a, b, c) => match (op, &nodes[a], &nodes[b], &nodes[c]) {
+                (op, Constant(a), Constant(b), Constant(c)) => {
+                    Some(Constant(Value::ternary_op(op, *a, *b, *c)?))
+                }
+                (Choose, Constant(flag), left, right) => {
+                    if flag.boolean()? {
+                        Some(*left)
                     } else {
-                        (None, Interval::unary_op(op, values[input])?)
+                        Some(*right)
                     }
                 }
-            },
-            Binary(op, li, ri) => match fold_binary_op(op, li, ri, nodes)? {
-                Some(folded) => (
-                    Some(folded),
-                    match &folded {
-                        Constant(value) => Interval::from_value(*value)?,
-                        Symbol(label) => vars
-                            .get(&label)
-                            .copied()
-                            .ok_or(Error::VariableNotFound(*label))?,
-                        Unary(op, input) => Interval::unary_op(*op, values[*input])?,
-                        Binary(op, lhs, rhs) => {
-                            Interval::binary_op(*op, values[*lhs], values[*rhs])?
-                        }
-                        Ternary(op, a, b, c) => {
-                            Interval::ternary_op(*op, values[*a], values[*b], values[*c])?
-                        }
-                    },
-                ),
-                None => {
-                    enum Choice {
-                        Left,
-                        Right,
-                        Custom(Node, Interval),
-                        None,
-                    }
-                    let choice = match (op, values[li], values[ri]) {
-                        (Add, Interval::Scalar(ileft), Interval::Scalar(iright)) => {
-                            match (ileft.is_singleton(), iright.is_singleton()) {
-                                (true, true) => Choice::Custom(
-                                    Constant(Scalar(ileft.inf() + iright.inf())),
-                                    Interval::Scalar(ileft + iright),
-                                ),
-                                (true, false) => todo!(),
-                                (false, true) => todo!(),
-                                (false, false) => todo!(),
-                            }
-                        }
-                        (Subtract, Interval::Scalar(_), Interval::Scalar(iright)) => todo!(),
-                        (Multiply, Interval::Scalar(ileft), Interval::Scalar(iright)) => {
-                            match (ileft.is_singleton(), iright.is_singleton()) {
-                                (true, true) => Choice::Custom(
-                                    Constant(Scalar(ileft.inf() * iright.inf())),
-                                    Interval::Scalar(ileft * iright),
-                                ),
-                                (true, false) => todo!(),
-                                (false, true) => todo!(),
-                                (false, false) => todo!(),
-                                _ => Choice::None,
-                            }
-                        }
-                        (Divide, Interval::Scalar(ileft), Interval::Scalar(iright)) => todo!(),
-                        (Pow, Interval::Scalar(ileft), Interval::Scalar(iright)) => todo!(),
-                        (Min, Interval::Scalar(ileft), Interval::Scalar(iright)) => todo!(),
-                        (Max, Interval::Scalar(ileft), Interval::Scalar(iright)) => todo!(),
-                        (Remainder, Interval::Scalar(ileft), Interval::Scalar(iright)) => todo!(),
-                        (Less, Interval::Scalar(ileft), Interval::Scalar(iright)) => todo!(),
-                        (LessOrEqual, Interval::Scalar(ileft), Interval::Scalar(iright)) => {
-                            todo!()
-                        }
-                        (Equal, Interval::Scalar(ileft), Interval::Scalar(iright)) => todo!(),
-                        (NotEqual, Interval::Scalar(ileft), Interval::Scalar(iright)) => todo!(),
-                        (Greater, Interval::Scalar(ileft), Interval::Scalar(iright)) => todo!(),
-                        (GreaterOrEqual, Interval::Scalar(ileft), Interval::Scalar(iright)) => {
-                            todo!()
-                        }
-                        (And, Interval::Boolean(_, _), Interval::Boolean(_, _)) => todo!(),
-                        (Or, Interval::Boolean(_, _), Interval::Boolean(_, _)) => todo!(),
-                        _ => return Err(Error::TypeMismatch),
-                    };
-                    match choice {
-                        Choice::Left => (Some(nodes[li]), values[li]),
-                        Choice::Right => (Some(nodes[ri]), values[ri]),
-                        Choice::Custom(node, value) => (Some(node), value),
-                        Choice::None => (None, Interval::binary_op(op, values[li], values[ri])?),
-                    }
-                }
-            },
-            Ternary(op, a, b, c) => match fold_ternary_op(op, a, b, c, nodes)? {
-                Some(folded) => (
-                    Some(folded),
-                    match &folded {
-                        Constant(value) => Interval::from_value(*value)?,
-                        Symbol(label) => vars
-                            .get(&label)
-                            .copied()
-                            .ok_or(Error::VariableNotFound(*label))?,
-                        Unary(op, input) => Interval::unary_op(*op, values[*input])?,
-                        Binary(op, lhs, rhs) => {
-                            Interval::binary_op(*op, values[*lhs], values[*rhs])?
-                        }
-                        Ternary(op, a, b, c) => {
-                            Interval::ternary_op(*op, values[*a], values[*b], values[*c])?
-                        }
-                    },
-                ),
-                None => (
-                    None,
-                    Interval::ternary_op(op, values[a], values[b], values[c])?,
-                ),
+                _ => None,
             },
         };
         if let Some(node) = folded {
             nodes[index] = node;
         }
-        values.push(value);
     }
     Ok(())
 }
@@ -285,7 +85,7 @@ impl Tree {
     /// the appropriate `Error` is returned.
     pub fn fold(self) -> Result<Tree, Error> {
         let (mut nodes, dims) = self.take();
-        fold(&mut nodes)?;
+        fold_nodes(&mut nodes)?;
         Tree::from_nodes(nodes, dims)
     }
 }
@@ -293,8 +93,7 @@ impl Tree {
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::{dedup::Deduplicater, deftree, prune::Pruner, test::compare_trees, tree::min};
-    use rand::{rngs::StdRng, SeedableRng};
+    use crate::{deftree, prune::Pruner, test::compare_trees};
 
     #[test]
     fn t_sconstant_folding_0() {
@@ -544,59 +343,5 @@ mod test {
             .prune(&mut pruner)
             .unwrap()
             .equivalent(&deftree!(if (> x 0) x (-x)).unwrap()),);
-    }
-
-    const RADIUS_RANGE: (f64, f64) = (0.2, 2.);
-    const X_RANGE: (f64, f64) = (0., 100.);
-    const Y_RANGE: (f64, f64) = (0., 100.);
-    const Z_RANGE: (f64, f64) = (0., 100.);
-    const N_SPHERES: usize = 5000;
-    const N_QUERIES: usize = 5000;
-
-    fn sample_range(range: (f64, f64), rng: &mut StdRng) -> f64 {
-        use rand::Rng;
-        range.0 + rng.random::<f64>() * (range.1 - range.0)
-    }
-
-    fn sphere_union() -> Tree {
-        let mut rng = StdRng::seed_from_u64(42);
-        let mut make_sphere = || -> Result<Tree, Error> {
-            deftree!(- (sqrt (+ (+
-                                 (pow (- x (const sample_range(X_RANGE, &mut rng))) 2)
-                                 (pow (- y (const sample_range(Y_RANGE, &mut rng))) 2))
-                              (pow (- z (const sample_range(Z_RANGE, &mut rng))) 2)))
-                     (const sample_range(RADIUS_RANGE, &mut rng)))
-        };
-        let mut tree = make_sphere();
-        for _ in 1..N_SPHERES {
-            tree = min(tree, make_sphere());
-        }
-        let tree = tree.unwrap();
-        assert_eq!(tree.dims(), (1, 1));
-        tree
-    }
-
-    fn _t_prune() {
-        let mut rng = StdRng::seed_from_u64(234);
-        let queries: Vec<[f64; 3]> = (0..N_QUERIES)
-            .map(|_| {
-                [
-                    sample_range(X_RANGE, &mut rng),
-                    sample_range(Y_RANGE, &mut rng),
-                    sample_range(Z_RANGE, &mut rng),
-                ]
-            })
-            .collect();
-        let tree = {
-            let mut dedup = Deduplicater::new();
-            let mut pruner = Pruner::new();
-            sphere_union()
-                .fold()
-                .unwrap()
-                .deduplicate(&mut dedup)
-                .unwrap()
-                .prune(&mut pruner)
-                .unwrap()
-        };
     }
 }
