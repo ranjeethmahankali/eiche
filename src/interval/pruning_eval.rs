01cdc505eea2e9c44d9081f7b4e95fa60001d58b
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2025-02-22 23:04:17 -0500

    Partial progress

--- src/interval/pruning_eval.rs
+++ src/interval/pruning_eval.rs
@@ -1,11 +1,10 @@
-use std::collections::BTreeMap;
-
+use super::Interval;
 use crate::{
+    compile::{compile, CompileCache, CompileOutput},
     eval::ValueType,
     tree::{Node, Tree},
 };
-
-use super::Interval;
+use std::{collections::BTreeMap, ops::Range};
 
 pub struct PruningEvaluator<T>
 where
@@ -14,14 +13,48 @@ where
     ops: Vec<(Node, usize)>,
     regs: Vec<T>,
     vars: Vec<(char, T)>,
+    num_roots: usize,
     root_regs: Vec<usize>,
     outputs: Vec<T>,
-    bounds: BTreeMap<char, Interval>,
+    bounds: Vec<BTreeMap<char, Interval>>, // Bounds stored like a stack.
+    op_ranges: Vec<Range<usize>>,
+    current_depth: usize,
 }
 
 impl<T> PruningEvaluator<T>
 where
     T: ValueType,
 {
-    pub fn new(tree: &Tree, bounds: BTreeMap<char, Interval>) {}
+    pub fn new(
+        tree: &Tree,
+        divisions: usize,
+        estimated_depth: usize,
+        bounds: BTreeMap<char, Interval>,
+    ) -> Self {
+        let num_roots = tree.num_roots();
+        let mut ops = Vec::with_capacity(tree.len() * estimated_depth);
+        let mut root_regs = Vec::with_capacity(num_roots * estimated_depth);
+        let mut cache = CompileCache::default();
+        let num_regs = compile(
+            tree.nodes(),
+            tree.root_indices(),
+            &mut cache,
+            CompileOutput {
+                ops: &mut ops,
+                out_regs: &mut root_regs,
+            },
+        );
+        debug_assert_eq!(root_regs.len(), num_roots);
+        PruningEvaluator {
+            ops,
+            regs: vec![T::from_scalar(0.).unwrap(); num_regs],
+            vars: Vec::new(),
+            num_roots,
+            root_regs,
+            outputs: todo!(),
+            bounds: todo!(),
+            op_ranges: todo!(),
+            current_depth: todo!(),
+        }
+    }
 }
