d498c5e043d83e1ec8aa40bb447ba92553b7b343
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2025-02-23 02:52:59 -0500

    Use a nested vector datastructure to simplify the logic

--- src/interval/pruning_eval.rs
+++ src/interval/pruning_eval.rs
@@ -6,14 +6,95 @@ use crate::{
     interval::fold::fold_for_interval,
     tree::{Node, Tree, Value},
 };
-use std::{collections::BTreeMap, ops::Range};
+use std::collections::BTreeMap;
+
+struct Stack<T>
+where
+    T: Clone,
+{
+    buf: Vec<T>,
+    offsets: Vec<usize>,
+}
+
+impl<T> Stack<T>
+where
+    T: Clone,
+{
+    pub fn new() -> Self {
+        Self {
+            buf: Vec::new(),
+            offsets: Vec::new(),
+        }
+    }
+
+    pub fn with_capacity(nslices: usize, ntotal: usize) -> Self {
+        Self {
+            buf: Vec::with_capacity(ntotal),
+            offsets: Vec::with_capacity(nslices),
+        }
+    }
+
+    pub fn start_slice(&mut self) {
+        self.offsets.push(self.buf.len());
+    }
+
+    pub fn pop_slice(&mut self) {
+        match self.offsets.pop() {
+            Some(last) => self.buf.truncate(last),
+            None => {}
+        }
+    }
+
+    pub fn push_slice(&mut self, vals: &[T]) {
+        self.start_slice();
+        self.buf.extend_from_slice(vals)
+    }
+
+    pub fn push_iter(&mut self, vals: impl Iterator<Item = T>) {
+        self.start_slice();
+        self.buf.extend(vals);
+    }
+
+    pub fn last_slice(&self) -> Option<&[T]> {
+        self.offsets.last().map(|last| &self.buf[*last..])
+    }
+
+    pub fn borrow_mut(&mut self) -> &mut Vec<T> {
+        self.start_slice();
+        &mut self.buf
+    }
+}
+
+impl<T> Default for Stack<T>
+where
+    T: Clone + Default,
+{
+    fn default() -> Self {
+        Self {
+            buf: Default::default(),
+            offsets: Default::default(),
+        }
+    }
+}
+
+impl<T> From<&[T]> for Stack<T>
+where
+    T: Clone,
+{
+    fn from(value: &[T]) -> Self {
+        let mut out = Self::with_capacity(1, value.len());
+        out.start_slice();
+        out.push_slice(value);
+        out
+    }
+}
 
 pub struct PruningEvaluator<T>
 where
     T: ValueType,
 {
-    nodes: Vec<Node>,
-    ops: Vec<(Node, usize)>,
+    nodes: Stack<Node>,
+    ops: Stack<(Node, usize)>,
     val_regs: Vec<T>,
     interval_regs: Vec<Interval>,
     vars: Vec<(char, T)>,
@@ -22,8 +103,6 @@ where
     val_outputs: Vec<T>,
     interval_outputs: Vec<Interval>,
     bounds: Vec<BTreeMap<char, Interval>>, // Bounds stored like a stack.
-    node_ranges: Vec<Range<usize>>,
-    op_ranges: Vec<Range<usize>>,
     interval_indices: Vec<usize>,
     intervals_per_level: usize,
     divisions: f64,
@@ -61,7 +140,7 @@ where
         bounds: BTreeMap<char, Interval>,
     ) -> Self {
         let num_roots = tree.num_roots();
-        let mut ops = Vec::with_capacity(tree.len() * estimated_depth);
+        let mut ops = Stack::with_capacity(estimated_depth, tree.len() * estimated_depth);
         let mut root_regs = Vec::with_capacity(num_roots * estimated_depth);
         let mut cache = CompileCache::default();
         let num_regs = compile(
@@ -69,15 +148,14 @@ where
             tree.root_indices(),
             &mut cache,
             CompileOutput {
-                ops: &mut ops,
+                ops: ops.borrow_mut(),
                 out_regs: &mut root_regs,
             },
         );
         debug_assert_eq!(root_regs.len(), num_roots);
-        let num_ops = ops.len();
         let num_bounds = bounds.len();
         PruningEvaluator {
-            nodes: tree.nodes().to_vec(),
+            nodes: tree.nodes().into(),
             ops,
             val_regs: vec![T::default(); num_regs],
             interval_regs: vec![Interval::default(); num_regs],
@@ -87,8 +165,6 @@ where
             val_outputs: vec![T::from_scalar(0.).unwrap(); num_roots],
             interval_outputs: vec![Interval::Scalar(inari::Interval::ENTIRE); num_roots],
             bounds: vec![bounds],
-            node_ranges: vec![0..tree.len()],
-            op_ranges: vec![0..num_ops],
             interval_indices: vec![0],
             intervals_per_level: divisions.pow(num_bounds as u32),
             divisions: divisions as f64,
@@ -126,8 +202,8 @@ where
             }
         };
         if let Err(e) = fold_for_interval(
-            match self.node_ranges.last() {
-                Some(range) => &self.nodes[range.clone()],
+            match self.nodes.last_slice() {
+                Some(nodes) => nodes,
                 None => return PruningState::Failure(PruningError::UnexpectedEmptyState.into()),
             },
             &bounds,
@@ -135,23 +211,22 @@ where
         ) {
             return PruningState::Failure(e);
         }
-        let nnodes_before = self.nodes.len();
-        self.nodes.extend(self.temp_nodes.drain(..));
-        let node_range = nnodes_before..self.nodes.len();
+        self.nodes.push_iter(self.temp_nodes.drain(..));
         self.bounds.push(bounds);
         self.interval_indices.push(0);
-        let nops_before = self.ops.len();
+        let nodes = match self.nodes.last_slice() {
+            Some(nodes) => nodes,
+            None => return PruningState::Failure(PruningError::UnexpectedEmptyState.into()),
+        };
         let num_regs = compile(
-            &self.nodes[node_range.clone()],
-            (node_range.len() - self.num_roots)..node_range.len(),
+            nodes,
+            (nodes.len() - self.num_roots)..nodes.len(),
             &mut self.compile_cache,
             CompileOutput {
-                ops: &mut self.ops,
+                ops: self.ops.borrow_mut(),
                 out_regs: &mut self.root_regs,
             },
         );
-        self.node_ranges.push(node_range);
-        self.op_ranges.push(nops_before..self.ops.len());
         self.val_regs.resize(num_regs, T::default());
         self.interval_regs.resize(num_regs, Interval::default());
         todo!("Go one level deeper");
