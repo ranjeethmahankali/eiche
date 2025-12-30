17333050b51f478fccda9ea82c109994a3623a4a
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2025-12-30 11:04:11 -0500

    Better printing for JIT compilation errors

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
+use std::fmt::Debug;
+
+#[derive(Clone, PartialEq, Eq)]
 pub enum Error {
     /// Nodes are not in a valid topological order.
     WrongNodeOrder,
@@ -42,4 +44,49 @@ pub enum Error {
     CannotCreateJitModule,
     CannotCompileIntrinsic(&'static str),
     JitCompilationError(String),
+}
+
+impl Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Error::*;
+        match self {
+            WrongNodeOrder => write!(f, "WrongNodeOrder"),
+            CyclicGraph => write!(f, "CyclicGraph"),
+            ContainsNaN => write!(f, "ContainsNaN"),
+            EmptyTree => write!(f, "EmptyTree"),
+            DependentRootNodes => write!(f, "DependentRootNodes"),
+            InvalidRoots => write!(f, "InvalidRoots"),
+            DimensionMismatch(a, b) => f
+                .debug_tuple("DimensionMismatch")
+                .field(a)
+                .field(b)
+                .finish(),
+            InvalidDimensions => write!(f, "InvalidDimensions"),
+            TypeMismatch => write!(f, "TypeMismatch"),
+            InvalidInterval => write!(f, "InvalidInterval"),
+            IndexOutOfBounds(a, b) => f.debug_tuple("IndexOutOfBounds").field(a).field(b).finish(),
+            VariableNotFound(label) => f.debug_tuple("VariableNotFound").field(label).finish(),
+            InvalidTemplateCapture => write!(f, "InvalidTemplateCapture"),
+            UnboundTemplateSymbol => write!(f, "UnboundTemplateSymbol"),
+            CannotComputeSymbolicDerivative => write!(f, "CannotComputeSymbolicDerivative"),
+            CannotComputeNumericDerivative => write!(f, "CannotComputeNumericDerivative"),
+            InputSizeMismatch(actual, expected) => f
+                .debug_tuple("InputSizeMismatch")
+                .field(actual)
+                .field(expected)
+                .finish(),
+            OutputSizeMismatch(actual, expected) => f
+                .debug_tuple("OutputSizeMismatch")
+                .field(actual)
+                .field(expected)
+                .finish(),
+            CannotCreateJitModule => write!(f, "CannotCreateJitModule"),
+            CannotCompileIntrinsic(name) => {
+                f.debug_tuple("CannotCompileIntrinsic").field(name).finish()
+            }
+            JitCompilationError(message) => {
+                write!(f, "JitCompilationError: \n{message}")
+            }
+        }
+    }
 }
